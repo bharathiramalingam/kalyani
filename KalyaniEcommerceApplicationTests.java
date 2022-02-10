3dae16739b7be4996ce6ad23ec66f5cafd909fc8
bharathiramalingam <99392496+bharathiramalingam@users.noreply.github.com>
2022-02-10 16:04:00 +0530

    test

---
+++ KalyaniEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalyani.ecommerce;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalyaniEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
